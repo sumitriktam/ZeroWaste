87227e0a20a5607b38ba581e40d64ab12d80d084
pradeep149 <npradeep9573@gmail.com>
2024-02-29 13:35:53 +0530

    updated ui and fixed bugs

--- admin_mod/urls.py
+++ admin_mod/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("login", views.login),
     path("register", views.register),
     path("adminPanel", views.adminPanel),
-    path("wait", views.waitingPage)
+    path("wait", views.waitingPage),
+    path('approve_user/<int:user_id>/', views.approve_user, name='approve_user'),
+    path('reject_user/<int:user_id>/', views.reject_user, name='reject_user')
 ]
