89ace31fb6ba4bf6569359a64543c28c62cc7a1b
pradeep149 <npradeep9573@gmail.com>
2024-03-02 19:14:10 +0530

    leaderboard is added and fixed register page

--- admin_mod/urls.py
+++ admin_mod/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("adminPanel", views.adminPanel),
     path("wait", views.waitingPage),
     path('approve_user/<int:user_id>/', views.approve_user, name='approve_user'),
-    path('reject_user/<int:user_id>/', views.reject_user, name='reject_user')
+    path('reject_user/<int:user_id>/', views.reject_user, name='reject_user'),
+    path('check_user_existence/', views.check_user_existence, name='check_user_existence')
+
 ]
