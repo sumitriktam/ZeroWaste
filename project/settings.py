a279c2262c63a61fb602a8236078b44b9c3b2329
pradeep149 <npradeep9573@gmail.com>
2024-03-06 00:46:14 +0530

    forgot password functinality added

--- project/settings.py
+++ project/settings.py
@@ -139,5 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nethetipradeep.20.it@anits.edu.in'
-EMAIL_HOST_PASSWORD = 'ASDasd@123'
+
+#enter your email and password here
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
