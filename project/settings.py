267c47706e06105a8a65c1774e51fdba949a0b2b
harinidumpa <dumpaharini.20.csm@gmail.com>
2024-03-28 04:04:47 +0530

    minor bug fixes

--- project/settings.py
+++ project/settings.py
@@ -90,9 +90,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'zerobase',
+        'NAME': 'KLS',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'Harini@123',
         'HOST':'localhost',
         'PORT':'3306',
     }
